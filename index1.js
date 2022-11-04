628dddcd9977f7951bf4c45dffdbaad97f09095f
Elvis <elviscruz45@gmail.com>
2022-11-03 22:34:43 -0500

    class 18

---
+++ index1.js
@@ -0,0 +1,30 @@
+//STATEFUL
+
+// import React, {useState} from "react"
+
+// const Button=()=>{
+//     const [name,setName]=useState("ELvis Cruz")
+
+//     return(
+//         <div>
+//             <h1> Hello World {name}</h1>
+//         </div>
+//     )
+// }
+
+
+// STATELESS
+
+
+// import React from "react"
+
+// const Button=()=>{
+
+//     return(
+//         <div>
+//             <h1> Hello World {name}</h1>
+//         </div>
+//     )
+// }
+
+
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const NotFound =()=>{
+    return (
+        <div>
+            No he encontrado nada, error 404, ni miel;
+        </div>
+    )
+}
+
+export default NotFound
