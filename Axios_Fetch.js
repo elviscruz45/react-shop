89030612fd3bf265c6a3a5a8fbdfea2f84adcff4
Elvis <elviscruz45@gmail.com>
2022-11-04 13:17:59 -0500

    class 20

---
+++ Axios_Fetch.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const API="https://api.escuelajs.co/api/v1/products"
+
+
+fetch(API).then((response)=>{
+    //code handling the response
+}).catch((error)=>{
+    //Code for handing the error
+})
+
+
+axios.get("url")
